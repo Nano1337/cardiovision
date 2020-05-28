425ee7ef4c3e763fbd91c893e9217e0cfb1dd8ed
Haoli <haoliyin2002@gmail.com>
2020-05-28 17:48:01 -0500

    Initial pages

---
+++ src/components/Landing/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => (
+  <div>
+    <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+            <h1>Landing Banner</h1>
+            <p>along with rest of html page from bootstrap template</p>
+        </div>
+    </div>
+  </div>
+);
+
+export default Landing;
